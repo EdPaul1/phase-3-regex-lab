147cd63901714d274ccb56efc709490deece0786
EdPaul1 <saidarajab101@gmail.com>
2023-05-25 17:45:40 +0300

    regex lab

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\A\(\d{3}\)\d{3}-\d{4}\z/) != nil
+  phone.match(/\A(\(\d{3}\)|\d{3})[-\s]?\d{3}[-\s]?\d{4}\z/) != nil
 end
+
